bf34a72b2cbe7ba82bfdac9135aa416e314ec218
Thorben Ziemek <thorben@ziemek.de>
2016-03-09 13:34:39 +0100

    API-766 cleanup / eslint

--- esLogAdapter.js
+++ esLogAdapter.js
@@ -1,23 +1,26 @@
+'use strict';
+
 /* Creates a prototype for a logger as used by elasticsearch. Directs logs to the provided bunyan logger */
 module.exports = {
 
-  createEsLogAdapter: function(log) {
-    return function(config) {
-      this.error = log.error.bind(log);
-      this.warning = log.warn.bind(log);
-      this.info = log.info.bind(log);
-      this.debug = log.debug.bind(log);
-      this.trace = function(method, requestUrl, body, responseBody, responseStatus) {
-        return log.trace({
-          method: method,
-          requestUrl: requestUrl,
-          body: body,
-          responseBody: responseBody,
-          responseStatus: responseStatus
-        });
-      };
-      this.close = function() {};
-      return this;
+    createEsLogAdapter: function (log) {
+        return function () {
+            this.error = log.error.bind(log);
+            this.warning = log.warn.bind(log);
+            this.info = log.info.bind(log);
+            this.debug = log.debug.bind(log);
+            // eslint-disable-next-line max-params
+            this.trace = function (method, requestUrl, body, responseBody, responseStatus) {
+                return log.trace({
+                    method: method,
+                    requestUrl: requestUrl,
+                    body: body,
+                    responseBody: responseBody,
+                    responseStatus: responseStatus
+                });
+            };
+            this.close = function () {};
+            return this;
+        };
     }
-  }
-}
+};
--- index.js
+++ index.js
@@ -1,150 +1,150 @@
 'use strict';
 
-var elasticsearch = require('elasticsearch'),
-	esLogAdapter = require('./esLogAdapter'),
-	_ = require('lodash'),
-	_api = null;
+var
+elasticsearch = require('elasticsearch'),
+esLogAdapter = require('./esLogAdapter'),
+_ = require('lodash');
 
-var DataSource = module.exports = function(api, config) {
-	// config: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
-	this.api = _api = api;
-	this.client = new elasticsearch.Client({
-		hosts: config.hosts,
-		log: esLogAdapter.createEsLogAdapter(api.log)
-	});
+var DataSource = module.exports = function (api, config) {
+    // config: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
+    this.api = api;
+    this.client = new elasticsearch.Client({
+        hosts: config.hosts,
+        log: esLogAdapter.createEsLogAdapter(api.log)
+    });
 };
 
 DataSource.prototype.prepare = function () {};
 
 /**
- * @param {Object} request
- * @param {Function} callback
- */
+* @param {Object} request
+* @param {Function} callback
+*/
 DataSource.prototype.process = function (request, callback) {
-	//this.api.log.info(request, "flora-elasticsearch processes req");
-	var search = this.createSearchConfig(request);
-	this.api.log.info(search, "flora-elasticsearch created search request");
+    //this.api.log.info(request, "flora-elasticsearch processes req");
+    var search = this.createSearchConfig(request);
+    this.api.log.info(search, 'flora-elasticsearch created search request');
 
-	this.client.search(search, function(err, response) {
-		if (callback) {
-			var result = null;
-			if (!err && response && response.hits && response.hits.hits) {
-				var data = response.hits.hits.map(function(hit) {
-					hit._source._id = hit._id;
-					return hit._source;
-				})
-				
-				result = {
-					data: data
-				};
-			}
+    this.client.search(search, function (err, response) {
+        if (callback) {
+            var result = null;
+            if (!err && response && response.hits && response.hits.hits) {
+                var data = response.hits.hits.map(function (hit) {
+                    hit._source._id = hit._id;
+                    return hit._source;
+                });
 
-			callback(err, result);
-		}
-	});
+                result = {
+                    data: data
+                };
+            }
+
+            callback(err, result);
+        }
+    });
 };
 
-DataSource.prototype.createSearchConfig = function(request) {
-	var body = {};
-	if (request.filter) {
-		body.query = {};
-		body.query.filtered = {filter:createFilter(request.filter)}; 
-	}
-	if (!request.limit) request.limit = 1000000;
+DataSource.prototype.createSearchConfig = function (request) {
+    var body = {};
+    if (request.filter) {
+        body.query = {};
+        body.query.filtered = {filter: createFilter(request.filter)};
+    }
+    if (!request.limit) request.limit = 1000000;
     if (request.page) {
-    	body.from = (request.page-1) * request.limit;
+        body.from = (request.page - 1) * request.limit;
     }
     body.size = request.limit;
 
-	var search = {};
-	//search.fields = request.attributes;
-	search.index = request.esindex;
-	search.type = request.estype;
-	if (body) search.body = body;
+    var search = {};
+    //search.fields = request.attributes;
+    search.index = request.esindex;
+    search.type = request.estype;
+    if (body) search.body = body;
 
-	return search;
+    return search;
 };
 
 /**
- * @param {Function} callback
- */
+* @param {Function} callback
+*/
 DataSource.prototype.close = function (callback) {
     // TODO: implement
     if (callback) callback();
 };
 
 function createFilter(floraFilter) {
-	var orConditions = floraFilter.map(convertAndFilters);
+    var orConditions = floraFilter.map(convertAndFilters);
 
-	if (orConditions.length > 1) {
-		return {
-			or: orConditions
-		}
-	} else if (orConditions.length == 1) {
-		return orConditions[0];
-	} else {
-		return null;
-	}
+    if (orConditions.length > 1) {
+        return {
+            or: orConditions
+        };
+    } else if (orConditions.length === 1) {
+        return orConditions[0];
+    } else {
+        return null;
+    }
 }
 
 function convertAndFilters(andFilters) {
-	var byAttribute = _.groupBy(andFilters, function(filter) {
-		return filter.attribute;
-	});
+    var byAttribute = _.groupBy(andFilters, function (filter) {
+        return filter.attribute;
+    });
 
-	var andConditions = [];
-	_.forEach(byAttribute, function(filters, attribute) {
-		var f = combineFilters(filters, attribute);
-		if (_.isArray(f)) andConditions = andConditions.concat(f)
-		else if (f) andConditions.push(f)
-	});
+    var andConditions = [];
+    _.forEach(byAttribute, function (filters, attribute) {
+        var f = combineFilters(filters, attribute);
+        if (_.isArray(f)) andConditions = andConditions.concat(f);
+        else if (f) andConditions.push(f);
+    });
 
-	if (andConditions.length > 1) {
-		return {
-			and: andConditions
-		}
-	} else if (andConditions.length == 1) {
-		return andConditions[0]
-	} else {
-		return null;
-	}
+    if (andConditions.length > 1) {
+        return {
+            and: andConditions
+        };
+    } else if (andConditions.length === 1) {
+        return andConditions[0];
+    } else {
+        return null;
+    }
 }
 
 function combineFilters(conditions, attribute) {
-	var result = {};
+    var result = {};
 
-	_.forEach(conditions, function(condition) {
-		if (condition.operator == 'equal') {
-			if (attribute == '_id') {
-				result.ids = result.ids || {};
-				result.ids.values = result.ids.values || [];
-				result.ids.values.push(condition.value);
-			} else {
-				/*  term filter */
-				result.term = {}
-				result.term[attribute] = condition.value; 
-			}
-		} else if (condition.operator == 'greater') {
-			result.range = result.range || {};
-			result.range[attribute] = result.range[attribute] || {};
-			result.range[attribute].gt = condition.value;
-		} else if (condition.operator == 'greaterOrEqual') {
-			result.range = result.range || {};
-			result.range[attribute] = result.range[attribute] || {};
-			result.range[attribute].gte = condition.value;
-		} else if (condition.operator == 'less') {
-			result.range = result.range || {};
-			result.range[attribute] = result.range[attribute] || {};
-			result.range[attribute].lt = condition.value;
-		} else if (condition.operator == 'lessOrEqual') {
-			result.range = result.range || {};
-			result.range[attribute] = result.range[attribute] || {};
-			result.range[attribute].lte = condition.value;
-		} else {
-			throw new Error("not yet implemented: operator " + condition.operator);
-		}
-	});
+    _.forEach(conditions, function (condition) {
+        if (condition.operator === 'equal') {
+            if (attribute === '_id') {
+                result.ids = result.ids || {};
+                result.ids.values = result.ids.values || [];
+                result.ids.values.push(condition.value);
+            } else {
+                /*  term filter */
+                result.term = {};
+                result.term[attribute] = condition.value;
+            }
+        } else if (condition.operator === 'greater') {
+            result.range = result.range || {};
+            result.range[attribute] = result.range[attribute] || {};
+            result.range[attribute].gt = condition.value;
+        } else if (condition.operator === 'greaterOrEqual') {
+            result.range = result.range || {};
+            result.range[attribute] = result.range[attribute] || {};
+            result.range[attribute].gte = condition.value;
+        } else if (condition.operator === 'less') {
+            result.range = result.range || {};
+            result.range[attribute] = result.range[attribute] || {};
+            result.range[attribute].lt = condition.value;
+        } else if (condition.operator === 'lessOrEqual') {
+            result.range = result.range || {};
+            result.range[attribute] = result.range[attribute] || {};
+            result.range[attribute].lte = condition.value;
+        } else {
+            throw new Error('not yet implemented: operator ' + condition.operator);
+        }
+    });
 
-	return result;
+    return result;
 
 }
